807976561804869ae7bcc44ea28a8b563513f230
rfabreu <raabreugomes@gmail.com>
2022-05-24 14:13:14 -0400

    Create first function to store player's name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!");
+// THIS CREATES A FUNCTION NAMED "FIGHT"
+
+function fight() {
+    // THIS WILL PROMPT THE USER TO ENTER A NAME AND WILL STORE IT
+    var playerName = window.prompt("What is your robot's name?");
+    // THIS WILL LOG A MESSAGE TO THE DEVELOPER IN THE CONSOLE
+    console.log("Player's robot " + playerName + " is being set...");
+    // THESE WILL GIVE INITIAL GREETINGS TO THE PLAYERS
+    window.alert(`Welcome ${playerName} !`);
+    // THIS IS ANOTHER SYNTAX TO DO THE SAME AS THE PREVIOUS ALERT
+    window.alert("Your robot " + playerName + " is good to go!");
+    window.alert("The fight has begun!");
+}
+
+fight();
